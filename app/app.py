442d8d140a1cd1ade410460ded45c88624b8b83a
Mark McConnell <mark.mcconnell@futrli.com>
2019-01-03 11:55:46 +0000

    basic form added

--- app/app.py
+++ app/app.py
@@ -1,11 +1,20 @@
+# stdlib imports
+import json
+
 # third-party imports
 from flask import Flask
 from flask import render_template
+from flask import request
 
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
+@app.route('/')
+def index():
     return render_template('index.html')
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    return json.dumps(request.form)
