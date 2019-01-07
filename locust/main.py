ef4babf8a5cf59ac2bd6b3c50bb1e3caa0c38f79
Mark McConnell <mark.mcconnell@futrli.com>
2019-01-07 10:20:26 +0000

    add locust testing

---
+++ locust/main.py
@@ -0,0 +1,17 @@
+from locust import HttpLocust, TaskSet, task
+
+
+class WebsiteTasks(TaskSet):
+
+    @task
+    def add_users(self):
+        self.client.post("/add_records", {
+            "active_stream_name": "marks-new-stream",
+            "num_users": "100"
+        })
+
+
+class WebsiteUser(HttpLocust):
+    task_set = WebsiteTasks
+    min_wait = 5000
+    max_wait = 15000
